2de45f883259ab3c824293ea8ab2efde40a01693
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2017-03-16 15:05:45 -0600

    Added Library Updates

---
+++ src/com/mjg/controller/rest/NetflixController.java
@@ -0,0 +1,51 @@
+package com.mjg.controller.rest;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mjg.model.Movie;
+import com.mjg.model.Search;
+
+@RestController
+@RequestMapping(value = "/netflix", produces = "application/json")
+public class NetflixController {
+
+	@RequestMapping(value = "name", method = RequestMethod.POST)
+	public Movie getMoviesByName(@ModelAttribute("search") Search search) {
+		return new Movie();
+	}
+
+	@RequestMapping(value = "name", method = RequestMethod.GET)
+	public ResponseEntity<Object> testMovie() {
+		Movie movie = new Movie();
+		ObjectMapper mapper = new ObjectMapper();
+		// JSON from file to Object
+		try {
+			movie = mapper.readValue(new URL("http://netflixroulette.net/api/api.php?title=Breaking%20Bad"),
+					Movie.class);
+		} catch (JsonParseException e) {
+			return new ResponseEntity<Object>("Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+
+		} catch (JsonMappingException e) {
+			return new ResponseEntity<Object>("Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (MalformedURLException e) {
+			return new ResponseEntity<Object>("Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (IOException e) {
+			return new ResponseEntity<Object>("Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Object>(movie, HttpStatus.OK);
+	}
+}
---
+++ src/com/mjg/model/Movie.java
@@ -0,0 +1,114 @@
+package com.mjg.model;
+
+public class Movie {
+
+	private String unit;
+	private String show_id;
+	private String show_title;
+	private String release_year;
+	private String rating;
+	private String category;
+	private String show_cast;
+	private String poster;
+	private String mediatype;
+	private String runtime;
+	private String director;
+	private String summary;
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getShow_id() {
+		return show_id;
+	}
+
+	public void setShow_id(String show_id) {
+		this.show_id = show_id;
+	}
+
+	public String getShow_title() {
+		return show_title;
+	}
+
+	public void setShow_title(String show_title) {
+		this.show_title = show_title;
+	}
+
+	public String getRelease_year() {
+		return release_year;
+	}
+
+	public void setRelease_year(String release_year) {
+		this.release_year = release_year;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getShow_cast() {
+		return show_cast;
+	}
+
+	public void setShow_cast(String show_cast) {
+		this.show_cast = show_cast;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+	public String getMediatype() {
+		return mediatype;
+	}
+
+	public void setMediatype(String mediatype) {
+		this.mediatype = mediatype;
+	}
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+}
